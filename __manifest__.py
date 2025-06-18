d2a0e9211d51af2e91c83dba5c568edfe992ddba
ismael0302 <tadjuidjeismael@gmail.com>
2025-06-18 18:58:29 +0100

    mise en place du module

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Gestion Emploi du Temps',
+    'version': '1.0',
+    'category': 'Education',
+    'summary': "Module de gestion d\'emploi du temps",
+    'author': 'Ismael',
+    'depends': ['base'],
+    'data': [
+        # 'views/emploi_temps_views.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
